7a30997984d87cf0375691b77682633226b86ade
Gonzalo Palma <gonzalop0210@gmail.com>
2022-12-21 15:51:51 -0500

    Solucion 9 terminado

--- main.js
+++ main.js
@@ -10,33 +10,43 @@ const iconThre = document.querySelector('.arrow__3');
 const iconFour = document.querySelector('.arrow__4');
 const iconFive = document.querySelector('.arrow__5');
 
+const questionOne = document.querySelector('.question__1');
+const questionTwo = document.querySelector('.question__2');
+const questionThree = document.querySelector('.question__3');
+const questionFour = document.querySelector('.question__4');
+const questionFive = document.querySelector('.question__5');
 
 iconOne.addEventListener('click', mostrarOne);
 function mostrarOne() {
     replyOne.classList.toggle('mostrar');
     iconOne.classList.toggle('voltear')
+    questionOne.classList.toggle('oscuro');
 }
 
 iconTwo.addEventListener('click', mostrarTwo);
 function mostrarTwo() {
     replyTwo.classList.toggle('mostrar');
     iconTwo.classList.toggle('voltear');
+    questionTwo.classList.toggle('oscuro');
 }
 
 iconThre.addEventListener('click', mostrarThree);
 function mostrarThree() {
     replyThree.classList.toggle('mostrar');
     iconThre.classList.toggle('voltear');
+    questionThree.classList.toggle('oscuro');
 }
 
 iconFour.addEventListener('click', mostrarFour);
 function mostrarFour() {
     replyFour.classList.toggle('mostrar');
     iconFour.classList.toggle('voltear');
+    questionFour.classList.toggle('oscuro');
 }
 
 iconFive.addEventListener('click', mostrarFive);
 function mostrarFive() {
     replyFive.classList.toggle('mostrar');
     iconFive.classList.toggle('voltear');
+    questionFive.classList.toggle('oscuro');
 }
